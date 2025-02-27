a41647ad5c4e4a242e2809f9449a6b9104dd9b12
김효준 <156153822+gywns2zang9@users.noreply.github.com>
2025-02-27 14:22:08 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 152 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,23 @@
+N, K = map(int, input().split())
+
+q = [[N, 0]]
+visited = [0] * 100001
+
+while len(q) > 0: #q가 남아있으면
+    now, cnt = q.pop(0)
+    if now == K:
+        print(cnt)
+        break
+
+    else:
+        if 0 <= now*2 <= 100000 and visited[now*2] == 0:
+            visited[now*2] = 1
+            q.append([now*2, cnt])        
+        
+        if 0 <= now-1 <= 100000 and visited[now-1] == 0:
+            visited[now-1] = 1
+            q.append([now-1, cnt+1])
+
+        if 0 <= now + 1 <= 100000 and visited[now+1] == 0:
+            visited[now+1] = 1
+            q.append([now+1, cnt+1])
