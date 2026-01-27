a7bcaadc4c29c597bf147ebffbbddb79d8599e02
김효준 <156153822+gywns2zang9@users.noreply.github.com>
2026-01-27 16:03:06 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,15 +1,15 @@
 def solution(n, lost, reserve):
-    answer = 0
+    answer = n - len(lost) # 체육복이 있는 학생 수
     
-    # 도난 당했지만 여벌 체육복이 있는 경우 제외
-    real_lost = set(lost) - set(reserve)
-    real_reserve = set(reserve) - set(lost)
-    
-    for rr in real_reserve:
-        if rr-1 in real_lost:
-            real_lost.remove(rr-1)
-
-        elif rr+1  in real_lost:
-            real_lost.remove(rr+1)
-    answer = n - len(real_lost)
+    for l in lost:
+        if l in reserve:
+            reserve.remove(l-1)
+            answer += 1
+            
+        elif l > 1 and l-1 in reserve:
+            reserve.remove(l-1)
+            answer += 1
+        elif n > l and l+1 in reserve:
+            reserve.remove(l+1)
+            answer += 1
     return answer
